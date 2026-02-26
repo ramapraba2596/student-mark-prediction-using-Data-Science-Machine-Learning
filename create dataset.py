f955d9dfe23866b67193c141307ba03019b47b46
ramapraba2596 <kramapraba2596@gmail.com>
2026-02-26 17:39:25 +0530

    Delete create dataset.py

--- create dataset.py
+++
@@ -1,9 +0,0 @@
-np.random.seed(42)
-
-data = pd.DataFrame({
-    "Study_Hours": np.random.randint(1, 7, 30),
-    "Attendance": np.random.randint(60, 100, 30),
-    "Marks": np.random.randint(40, 100, 30)
-})
-
-data.head()
